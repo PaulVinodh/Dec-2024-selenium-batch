a46527ee7a078705a93a15d9fdc6093161142b4d
PaulVinodh <70633890+PaulVinodh@users.noreply.github.com>
2024-12-24 18:42:37 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Chrome {
+  public static void main(String[] args) {
+	float versionOfType=91.0f;
+	String developerOfType="Google";
+	boolean isBetaOfType=false;
+	int releaseYear=2008;
+	char shortcutKeyOfType='C';
+	System.out.println(versionOfType+developerOfType+isBetaOfType+releaseYear+shortcutKeyOfType);
+	
+}
+}
